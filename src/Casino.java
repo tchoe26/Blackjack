71238dae97b0950377c6bd1cda61b4ef947cb407
tchoe26 <theodore_choe26@milton.edu>
2024-04-29 13:08:32 -0400

    Initial commit

---
+++ src/Casino.java
@@ -0,0 +1,10 @@
+public class Casino {
+    public static void main(String[] args) {
+        Casino c = new Casino();
+    }
+
+
+    public Casino(){
+        System.out.println("welcome to the casino");
+    }
+}
---
+++ src/Player.java
@@ -0,0 +1,5 @@
+public class Player {
+    int cardValue;
+    int hand;
+    String name;
+}
