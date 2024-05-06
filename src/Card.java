5568aeae454244cf45fe79ff64544ea9220973d4
tchoe26 <theodore_choe26@milton.edu>
2024-05-06 08:55:43 -0400

    created instance variables for key blackjack information

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,7 @@
 public class Card {
+    public Card(){
+        String suit;
+        String type;
+        int value;
+    }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,11 @@
 public class Player {
-    int cardValue;
-    int hand;
-    String name;
+    public Player() {
+        int cardValue;
+        int hand;
+        String name;
+        String playerType; //dealer vs. player - maybe make into a boolean
+        boolean isHit;
+    }
+
+
 }
