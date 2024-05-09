4633b5e244dc4e7e64b2ecc3d4c6ecb2f7161bbd
tchoe26 <theodore_choe26@milton.edu>
2024-05-09 09:05:11 -0400

    shuffle deck

--- src/Casino.java
+++ src/Casino.java
@@ -8,6 +8,12 @@ public class Casino {
 
     public Casino() {
         System.out.println("welcome to the casino");
+        makeDeck();
+        shuffleDeck();
+        printDeck();
+    }
+
+    public void makeDeck() {
         deck = new Card[52];
         int count=0;
 
@@ -15,8 +21,25 @@ public class Casino {
         for (int s=0; s<4; s++) {
             for (int t=0; t<13; t++) {
                 deck[count] = new Card(s,t);
-                deck[count].print();
+                count++;
             }
+        }
+    }
+
+    public void shuffleDeck() {
+        for (int x=0; x<52; x++) {
+            int randomIndex = (int)(Math.random()*52);
+          Card randomCard = deck[randomIndex];
+
+          Card temp = deck[x];
+          deck[x]=randomCard;
+          deck[randomIndex] = temp;
+        }
+
+    }
+    public void printDeck() {
+        for (int i=0; i<52; i++) {
+            deck[i].print();
         }
     }
 
