9fd160062c80ecf1b5ba744995bd5074f89456bc
tchoe26 <theodore_choe26@milton.edu>
2024-05-12 20:32:12 -0400

    made constructor and print method for the player

--- src/Player.java
+++ src/Player.java
@@ -1,14 +1,25 @@
+import java.util.Arrays;
+
 public class Player {
 
-    public int cardValue;
+    public int handValue;
     public Card[] hand;
     public String name;
     public String playerType; //dealer vs. player - maybe make into a boolean
     public boolean isHit;
-    public Player() {
+    public Player(String pName, Card[] pHand, String pPlayerType) {
+        name = pName;
+        hand = pHand;
+        playerType = pPlayerType;
 
+        for (int i=0; i<hand.length; i++) {
+            handValue+=hand[i].value;
+        }
 
     }
 
+    public void print() {
+        System.out.println("Player " + name + "is a" + playerType + "who has cards " + Arrays.toString(hand) + " equaling a value of " + handValue);
+    }
 
 }
