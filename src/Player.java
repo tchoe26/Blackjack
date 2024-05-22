316a2b268913ee5668ed5c3e17fa45a21bf7b6f7
tchoe26 <theodore_choe26@milton.edu>
2024-05-22 13:16:30 -0400

    added new array one bigger than hand - will probably change back later

--- src/Player.java
+++ src/Player.java
@@ -5,7 +5,7 @@ public class Player {
     public int handLength;
 
     public int handValue;
-    public Card[] hand = new Card[99];
+    public Card[] hand = new Card[2];
     public String name;
     public boolean isPlayer; //dealer vs. player - maybe make into a boolean
     public String decision;
@@ -30,6 +30,11 @@ public class Player {
     }
 
     public void addCard(Card c){
+        Card[] temp = new Card[hand.length + 1];
+        for (int i=0; i<hand.length; i++) {
+            temp[i] = hand[i];
+        }
+
         hand[numCards] = c;
         numCards = numCards+1;
         handValue += c.value;
